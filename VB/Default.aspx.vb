a709b20c267a8f0863c7e783aad7132a41045cfd
DevExpressExampleBot <fakeEmail>
2026-01-29 15:54:37 +0400

    Source auto update [skip ci]

--- VB/Default.aspx.vb
+++ VB/Default.aspx.vb
@@ -2,13 +2,13 @@
 Imports System
 
 Partial Public Class _Default
-    Inherits System.Web.UI.Page
+	Inherits System.Web.UI.Page
 
-    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
 
-    End Sub
+	End Sub
 
-    Protected Sub ASPxButton1_Click(ByVal sender As Object, ByVal e As EventArgs)
-        ASPxLabel1.Text = ASPxDropDownEdit1.KeyValue.ToString()
-    End Sub
+	Protected Sub ASPxButton1_Click(ByVal sender As Object, ByVal e As EventArgs)
+		ASPxLabel1.Text = ASPxDropDownEdit1.KeyValue.ToString()
+	End Sub
 End Class
